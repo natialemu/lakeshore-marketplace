405c4b39f6b812d2b671cec6a144a91e9a705a51
Nathnael Alemu <nathnael.alemu@yahoo.com>
2017-09-30 18:30:34 -0500

    implemented Bank account class

--- src/Domain/Account/AccountProfile/Finance/BankAccount.java
+++ src/Domain/Account/AccountProfile/Finance/BankAccount.java
@@ -1,4 +1,15 @@
 package Domain.Account.AccountProfile.Finance;
 
 public interface BankAccount {
+    String getBankName();
+    String getRoutingNumber();
+    String getAccountNumber();
+    String getSwiftCode();
+    String getAccountType();
+
+    void setBankName(String bankName);
+    void setRoutingNumber(String routingNumber);
+    void setAccountNumber(String accountNumber);
+    void setSwiftCode(String swiftCode);
+    void setAccountType(String accountType);
 }
