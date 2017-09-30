02153b8424d81f8aba4814d490c42b0b2ae95004
Nathnael Alemu <nathnael.alemu@yahoo.com>
2017-09-30 16:13:21 -0500

    implemented the contact info class

--- src/Domain/Account/AccountProfile/Contact/ContactInfo.java
+++ src/Domain/Account/AccountProfile/Contact/ContactInfo.java
@@ -1,4 +1,13 @@
 package Domain.Account.AccountProfile.Contact;
 
 public interface ContactInfo {
+    void setFullName(String fullName);
+    void setPhone(Phone phone);
+    void setEmail(String email);
+    void setSecurityQuestion(String securityQuestion);
+    void setSecurityQuestionAnswer(String securityQuestionAnswer);
+    void setLocation(Location location);
+    void setLocation(String city, String streetAddress, String state, int zipcode, String country, int pobox);
+    void setBirthDate(String birthDate);
+
 }
--- src/Domain/Account/AccountProfile/Contact/ContactInfoImpl.java
+++ src/Domain/Account/AccountProfile/Contact/ContactInfoImpl.java
@@ -1,4 +1,95 @@
 package Domain.Account.AccountProfile.Contact;
 
-public class ContactInfoImpl {
+
+/*
+  Class that represents the contact information of a user
+ */
+public class ContactInfoImpl implements ContactInfo{
+
+    private String fullName;
+    private Phone phone;
+    private String email;
+    private String securityQuestion;
+    private Location location;
+    private String birthDate;
+    private String securityQuestionAnswer;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Phone phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSecurityQuestion() {
+        return securityQuestion;
+    }
+
+    public void setSecurityQuestion(String securityQuestion) {
+        this.securityQuestion = securityQuestion;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    @Override
+    public void setLocation(String city, String streetAddress, String state, int zipcode, String country, int pobox) {
+        this.location = new LocationImpl(streetAddress,city,zipcode,state,country,pobox);
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getSecurityQuestionAnswer() {
+        return securityQuestionAnswer;
+    }
+
+    public void setSecurityQuestionAnswer(String securityQuestionAnswer) {
+        this.securityQuestionAnswer = securityQuestionAnswer;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactInfoImpl{" +
+                "fullName='" + fullName + '\'' +
+                ", phone=" + phone +
+                ", email='" + email + '\'' +
+                ", securityQuestion='" + securityQuestion + '\'' +
+                ", location=" + location +
+                ", birthDate='" + birthDate + '\'' +
+                ", securityQuestionAnswer='" + securityQuestionAnswer + '\'' +
+                '}';
+    }
+
+
+
+
+
 }
