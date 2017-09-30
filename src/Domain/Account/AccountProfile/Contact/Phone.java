4a89c65ef54326b471ea1c4d6a8586afbde24b59
Nathnael Alemu <nathnael.alemu@yahoo.com>
2017-09-30 00:59:50 -0500

    implemented Phone class of Account module

--- src/Domain/Account/AccountProfile/Contact/Phone.java
+++ src/Domain/Account/AccountProfile/Contact/Phone.java
@@ -1,4 +1,17 @@
 package Domain.Account.AccountProfile.Contact;
 
 public interface Phone {
+
+    void setWorkPhone(String workPhone);
+    void setHomePhone(String homePhone);
+    void setCellularPhone(String cellularPhone);
+    void setAreaCode(String areaCode);
+    void setCountryCode(String countryCode);
+
+    String getWorkPhone();
+    String getHomePhone();
+    String getCellularPhone();
+    String getAreaCode();
+    String getCountryCode();
+
 }
--- src/Domain/Account/AccountProfile/Contact/PhoneImpl.java
+++ src/Domain/Account/AccountProfile/Contact/PhoneImpl.java
@@ -1,4 +1,63 @@
 package Domain.Account.AccountProfile.Contact;
 
-public class PhoneImpl {
+public class PhoneImpl implements Phone{
+    private String workPhone;
+    private String homePhone;
+    private String cellularPhone;
+
+    public String getWorkPhone() {
+        return workPhone;
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneImpl{" +
+                "workPhone='" + workPhone + '\'' +
+                ", homePhone='" + homePhone + '\'' +
+                ", cellularPhone='" + cellularPhone + '\'' +
+                ", areaCode='" + areaCode + '\'' +
+                ", countryCode='" + countryCode + '\'' +
+                '}';
+    }
+
+    public void setWorkPhone(String workPhone) {
+        this.workPhone = workPhone;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public void setHomePhone(String homePhone) {
+        this.homePhone = homePhone;
+    }
+
+    public String getCellularPhone() {
+        return cellularPhone;
+    }
+
+    public void setCellularPhone(String cellularPhone) {
+        this.cellularPhone = cellularPhone;
+    }
+
+    public String getAreaCode() {
+        return areaCode;
+    }
+
+    public void setAreaCode(String areaCode) {
+        this.areaCode = areaCode;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    private String areaCode;
+    private String countryCode;
+
+
 }
