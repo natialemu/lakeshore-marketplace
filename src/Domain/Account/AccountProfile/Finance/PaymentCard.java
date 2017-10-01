6855a2cb2d999c4eaec71f25d3ac1fb26cae8e47
Nathnael Alemu <nathnael.alemu@yahoo.com>
2017-09-30 20:24:17 -0500

    implemented payment card class

--- src/Domain/Account/AccountProfile/Finance/PaymentCard.java
+++ src/Domain/Account/AccountProfile/Finance/PaymentCard.java
@@ -1,4 +1,15 @@
 package Domain.Account.AccountProfile.Finance;
 
 public interface PaymentCard {
+
+    void setCardHolderName(String cardHolderName);
+    void setCardNumber(String cardNumber);
+    void setCardExpirationDate(String cardExpirationDate);
+    void setCardSecurityNumber(int cardSecurityNumber);
+
+    String getCardHolderName();
+    String getCardNumber();
+    String getCardExpirationDate();
+    int getCardSecurityNumber();
+
 }
