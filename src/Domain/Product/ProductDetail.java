38a19a46044038aaea02ca3a1ec8c1d5b5a4bf64
Shaun <ljohnson24@luc.edu>
2017-09-30 21:44:44 -0500

    added behaviors to the product detail interface

--- src/Domain/Product/ProductDetail.java
+++ src/Domain/Product/ProductDetail.java
@@ -1,4 +1,25 @@
 package Domain.Product;
 
 public interface ProductDetail {
+	private String productName;
+	private long productID;
+	private double cost;
+	private String productType;
+	private String productTag;
+	private String productPhoto;
+	
+	void setProductName(String productName);
+	void setProductID(long productID);
+	void setProductCost(double cost);
+	void setProductType(String productType);
+	void setProductTag(String productTag);
+	void setProductPhoto(String Photo);
+	
+	String getProductName();
+	long getProductID();
+	double getProductCost();
+	String getProductType();
+	String getProductTag();
+	String getProductPhoto();
+	
 }
