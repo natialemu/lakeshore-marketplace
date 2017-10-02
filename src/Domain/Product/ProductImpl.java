fe8f6f89e21fb3983024f704893b3fc3606fc09f
ljohnson <ljohnson@luc.edu>
2017-10-01 22:18:55 -0500

    added to productimpl

--- src/Domain/Product/ProductImpl.java
+++ src/Domain/Product/ProductImpl.java
@@ -1,4 +1,23 @@
 package Domain.Product;
 
 public class ProductImpl {
+	private ProductDetail productDetail;
+	private Partner partner;
+	
+	public ProductDetail getProductDetail() {
+		return productDetail;
+	}
+	
+	public void setProductDetail(ProductDetail productDetail) {
+		this.productDetail = productDetail;
+	}
+	
+	public Partner getPartner() {
+		return this.partner;
+	}
+	
+	public void setPartner(Partner partner) {
+		this.partner = partner;
+	}
+	
 }
