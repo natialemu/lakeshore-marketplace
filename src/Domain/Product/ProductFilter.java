e55682adc480763505ef879740a56f827e4f4685
ljohnson <ljohnson@luc.edu>
2017-10-01 22:45:12 -0500

    product filter and search

--- src/Domain/Product/ProductFilter.java
+++ src/Domain/Product/ProductFilter.java
@@ -1,4 +1,7 @@
 package Domain.Product;
 
 public interface ProductFilter {
+	public List<Product> productFilter(List<Product> products);
+	public List<Product> productSearch(List<Product> products);
+	public List<Product> productSort(List<Product> products);
 }
