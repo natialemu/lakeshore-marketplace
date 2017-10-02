12a15f87d31f46ac44e1338cccc5048b03d475e8
Nathnael Alemu <nathnael.alemu@yahoo.com>
2017-10-02 02:15:35 -0500

    implemented order state and confirm order

--- src/Domain/Order/OrderState/DeliveredOrder.java
+++ src/Domain/Order/OrderState/DeliveredOrder.java
@@ -1,4 +1,42 @@
 package Domain.Order.OrderState;
 
-public class DeliveredOrder {
+import Domain.Order.Order;
+
+public class DeliveredOrder implements OrderState {
+    private Order order;
+
+    public DeliveredOrder(Order order){
+        this.order = order;
+    }
+
+    @Override
+    public boolean verifyPayment() {
+        return false;
+    }
+
+    @Override
+    public boolean verifyOrderContent() {
+        return false;
+    }
+
+    @Override
+    public boolean processPayment() {
+        return false;
+    }
+
+    @Override
+    public boolean shipOrder() {
+        return false;
+    }
+
+    @Override
+    public boolean cancelOrder() {
+        order.setState(order.getCancelledState());
+        return true;
+    }
+
+    @Override
+    public boolean sendConfirmation() {
+        return true;
+    }
 }
