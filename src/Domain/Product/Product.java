3b22213a3d278cbe4e79c916a064f2c6957b5be6
ljohnson <ljohnson@luc.edu>
2017-10-01 22:24:46 -0500

    Added behaviors to the product interface

--- src/Domain/Product/Product.java
+++ src/Domain/Product/Product.java
@@ -1,5 +1,9 @@
 package Domain.Product;
 
 public interface Product {
+	public ProductDetail getProductDetail();
+	public void setProductDetail(ProductDetail productDetail);
+	public Partner getPartner();
+	public void setPartner(Partner partner);
 	
 }
