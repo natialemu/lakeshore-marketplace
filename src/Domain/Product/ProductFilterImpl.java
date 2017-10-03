54f71a1d5e0f2ee8ecddd8b981d8302ce1bee628
ljohnson <ljohnson@luc.edu>
2017-10-02 23:29:51 -0500

    develop

--- src/Domain/Product/ProductFilterImpl.java
+++ src/Domain/Product/ProductFilterImpl.java
@@ -1,29 +1,4 @@
 package Domain.Product;
 
-public class ProductFilterImpl implements ProductFilter {
-	private String filterType;
-	private boolean order;
-	
-	
-	public void setFilterType(String filterType) {
-		this.filterType = filterType;
-	}
-	
-	public List<Product> productFilter(List<Product> products, String filterType, boolean order){
-		List<Product> productTypes = new ArrayList<Product>();
-		 
-		for (Product product : products) {
-			if (product.getProductType().equalsIgnoreCase(filterType)) {
-				productTypes.add(product);
-			}
-		}
-		return productTypes;
-	}
-	public List<Product> productSearch(List<Product> products){
-		return products
-	}
-	
-	public List<Product> productSort(List<Product> products){
-		return products
-	}	
+public class ProductFilterImpl {
 }
