c998ecaa9b20fd82473c230b3ada8f62a2b7589a
Shaun <ljohnson24@luc.edu>
2017-10-03 18:37:34 -0500

    modified productfactory
    added more method to the product factory

--- src/Domain/Product/ProductFactory.java
+++ src/Domain/Product/ProductFactory.java
@@ -4,9 +4,28 @@ public interface ProductFactory {
 	public boolean createProduct(String productName, double productCost, long productID, 
 	String productType, String productTag, String productPhoto);
 	
-	public boolean deleteProduct(Product product);
+	public boolean deleteProduct(productID);
 	
-	public boolean updateProduct(Product product, String productName, double productCost, 
-	long productID, String productType, String productTag, String productPhoto);
+	public boolean updateProductName(long productID, String productName);
+	
+	public boolean updateProductCost(long productID, double productCost);
+	
+	public boolean updateProductType(long productID, String productType);
+	
+	public boolean updateProductTag(long productID, String productTag);
+	
+	public boolean updateProductPhoto(long productID, String productPhoto);
+	
+	public List<Product> retrieveProductList();
+	
+	public List<Product> sortProductListByProductName(List<Product> product, boolean order);
+	
+	public List<Product> sortProductListByProductID(List<Product> product, boolean order);
+	
+	public List<Product> sortProductListByProductType(List<Product> product, boolean order);
+	
+	public List<Product> sortProductListByProductTag(List<Product> product, boolean order);
+	
+	public List<Product> sortProductListByProductCost(List<Product> product, boolean order);
 	
 }
