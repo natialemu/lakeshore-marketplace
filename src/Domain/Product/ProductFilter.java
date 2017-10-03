471e97809b2f56f20d0ca1920d624084f5b71260
Shaun Johnson <johnsonl@sandc.com>
2017-10-03 14:26:14 -0500

    feature/productfilterinterface
    *removed search behaviors
    *added missing behaviors

--- src/Domain/Product/ProductFilter.java
+++ src/Domain/Product/ProductFilter.java
@@ -1,7 +1,11 @@
 package Domain.Product;
 
 public interface ProductFilter {
-	public List<Product> productFilter(List<Product> products, String filterType, boolean order);
-	public List<Product> productSearch(List<Product> products, boolean order);
-	public List<Product> productSort(List<Product> products, boolean order);
+	public List<Product> productSortByName(List<Product> products, String sortName, boolean order);
+	public List<Product> productSortByID(List<Product> products, long sortID, boolean order);
+	public List<Product> productSortByCost(List<Product> products, double sortCost, boolean order);
+	public List<Product> productSortByTag(List<Product> products, String sortTag, boolean order);
+	public List<Product> productSortByType(List<Product> products, String sortType, boolean order);
+	public List<Product> productSortByPartnerName(List<Product> products, String sortPartnerName, boolean order);
+	public List<Product> productSortByPartnerID(List<Product> products, long sortPartnerID, boolean order);
 }
