8c6f1e65ae8abd7c3c58ea24deed0a9bb8c90ff3
Nathnael Alemu <nathnael.alemu@yahoo.com>
2017-10-02 21:24:54 -0500

    implemented contact-dao

--- src/Repository/Account/AccountProfileDAO/LocationDAOImpl.java
+++ src/Repository/Account/AccountProfileDAO/LocationDAOImpl.java
@@ -1,4 +1,21 @@
 package Repository.Account.AccountProfileDAO;
 
-public class LocationDAOImpl {
+import Domain.Account.AccountProfile.Contact.Location;
+
+public class LocationDAOImpl implements LocationDAO{
+    @Override
+    public boolean updateAddress(String streetAddress, String state, String city, int zipcode, String country, int pobox) {
+        return false;
+    }
+
+    @Override
+    public void createLocation(Location location) {
+
+    }
+
+    @Override
+    public Location getLocation(int zipcode) {
+
+        return null;
+    }
 }
