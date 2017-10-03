e8b2db88e3d5732f2a29a9f243d928df29948e52
ljohnson <ljohnson@luc.edu>
2017-10-02 23:16:24 -0500

    implementation for product factory class

--- src/Domain/Product/ProductFactoryImpl.java
+++ src/Domain/Product/ProductFactoryImpl.java
@@ -1,4 +1,27 @@
 package Domain.Product;
 
 public class ProductFactoryImpl {
+	private Product product;
+	private Filter filter;
+	private Partner partner;
+	
+	public boolean createProduct(String productName, double productCost, long productID, 
+		String productType, String productTag, String productPhoto){
+							
+		product = new ProductImpl();
+		return true;
+		
+	}
+	
+	public boolean deleteProduct(Product product){
+		//product delete implementation
+		return true;
+	}
+	
+	public boolean updateProduct(Product product, String productName, double productCost, 
+		long productID, String productType, String productTag, String productPhoto){
+		
+		product.setProductDetail(product, productName, productCost, productID, productType, productTag, productPhoto);
+		}
+	}
 }
