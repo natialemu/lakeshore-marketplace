63be277e1d14b7db706e5eccb4d95670e05ed8f7
ljohnson <ljohnson@luc.edu>
2017-10-02 23:22:47 -0500

    behavior for product filter

--- src/Domain/Product/ProductFilter.java
+++ src/Domain/Product/ProductFilter.java
@@ -1,4 +1,7 @@
 package Domain.Product;
 
 public interface ProductFilter {
+	public List<Product> productFilter(List<Product> products, String filterType, boolean order);
+	public List<Product> productSearch(List<Product> products, boolean order);
+	public List<Product> productSort(List<Product> products, boolean order);
 }
