4a8c83f251cefb16409b713f9dcada86ec85d47c
ljohnson <ljohnson@luc.edu>
2017-10-02 23:12:11 -0500

    behavior for product factory

--- src/Domain/Product/ProductFactory.java
+++ src/Domain/Product/ProductFactory.java
@@ -1,4 +1,12 @@
 package Domain.Product;
 
 public interface ProductFactory {
+	public boolean createProduct(String productName, double productCost, long productID, 
+	String productType, String productTag, String productPhoto);
+	
+	public boolean deleteProduct(Product product);
+	
+	public boolean updateProduct(Product product, String productName, double productCost, 
+	long productID, String productType, String productTag, String productPhoto);
+	
 }
