c1f10812cb0e433b38572bd92a2cb622956a9194
Shaun <ljohnson24@luc.edu>
2017-10-04 01:15:40 -0500

    product search interface
    redefined parameters of the interface methods

--- src/Domain/Product/ProductSearch.java
+++ src/Domain/Product/ProductSearch.java
@@ -2,11 +2,11 @@ package Domain.Product;
 
 public interface ProductSearch {
 	
-	public List<Product> productSearchByName(List<Product> products, String searchName, boolean order);
-	public Product productSearchByID(List<Product> products, long searchID, boolean order);
-	public List<Product> productSearchByCost(List<Product> products, double searchCost, boolean order);
-	public List<Product> productSearchByTag(List<Product> products, String searchTag, boolean order);
-	public List<Product> productSearchByType(List<Product> products, String searchType, boolean order);
-	public List<Product> productSearchByPartnerName(List<Product> products, String searchPartnerName, boolean order);
-	public List<Product> productSearchByPartnerID(List<Product> products, long searchPartnerID, boolean order);
+	public List<Product> productSearchByName(String searchName);
+	public Product productSearchByID(long searchID);
+	public List<Product> productSearchByCost(double searchCost);
+	public List<Product> productSearchByTag(String searchTag);
+	public List<Product> productSearchByType(String searchType);
+	public List<Product> productSearchByPartnerName(String searchPartnerName);
+	public List<Product> productSearchByPartnerID(long searchPartnerID);
 }
