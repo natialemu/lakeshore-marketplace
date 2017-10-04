89319fa2202897c5a59e919fb7be7bf7d9c28c0e
Shaun <ljohnson24@luc.edu>
2017-10-04 01:09:14 -0500

    Productfilter
    added filter behavior to the product filter interface

--- src/Domain/Product/ProductFilter.java
+++ src/Domain/Product/ProductFilter.java
@@ -1,11 +1,18 @@
 package Domain.Product;
 
 public interface ProductFilter {
-	public List<Product> productSortByName(List<Product> products, String sortName, boolean order);
-	public List<Product> productSortByID(List<Product> products, long sortID, boolean order);
-	public List<Product> productSortByCost(List<Product> products, double sortCost, boolean order);
-	public List<Product> productSortByTag(List<Product> products, String sortTag, boolean order);
-	public List<Product> productSortByType(List<Product> products, String sortType, boolean order);
-	public List<Product> productSortByPartnerName(List<Product> products, String sortPartnerName, boolean order);
-	public List<Product> productSortByPartnerID(List<Product> products, long sortPartnerID, boolean order);
+	public List<Product> productSortByName(List<Product> products,boolean order);
+	public List<Product> productSortByID(List<Product> products, boolean order);
+	public List<Product> productSortByCost(List<Product> products, boolean order);
+	public List<Product> productSortByTag(List<Product> products, boolean order);
+	public List<Product> productSortByType(List<Product> products, boolean order);
+	public List<Product> productSortByPartnerName(List<Product> products, boolean order);
+	public List<Product> productSortByPartnerID(List<Product> products, boolean order);
+	public List<Product> productFilterByName(List<Product> products, String filterName, boolean order);
+	public List<Product> productFilterByID(List<Product> products, long filterID, boolean order);
+	public List<Product> productFilterByCost(List<Product> products, double filterCost, boolean order);
+	public List<Product> productFilterByTag(List<Product> products, String filterTag, boolean order);
+	public List<Product> productFilterByType(List<Product> products, String filterType, boolean order);
+	public List<Product> productFilterByPartnerName(List<Product> products, String filterPartnerName, boolean order);
+	public List<Product> productFilterByPartnerID(List<Product> products, long filterPartnerID, boolean order);
 }
