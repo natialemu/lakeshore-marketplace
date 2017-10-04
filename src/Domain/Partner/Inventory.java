3f49b319b20e8d12b05035e51b0e63710bbe34c4
Shaun <ljohnson24@luc.edu>
2017-10-04 03:42:33 -0500

    partner inventory
    added behaviors

--- src/Domain/Partner/Inventory.java
+++ src/Domain/Partner/Inventory.java
@@ -1,4 +1,6 @@
 package Domain.Partner;
 
 public interface Inventory {
+	public Map<Product product,Integer index> getInventory();
+	public void setInventory(Map<Product product,Integer index> inventory);
 }
