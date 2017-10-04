85bdc9928cb397121822b8c13ec7b6cd71730f1f
Shaun <ljohnson24@luc.edu>
2017-10-04 01:18:31 -0500

    product search impl
    modified methods of the search implementation class

--- src/Domain/Product/ProductSearchImpl.java
+++ src/Domain/Product/ProductSearchImpl.java
@@ -1,127 +1,50 @@
 package Domain.Product;
 
 public class ProductSearchImpl implements ProductSearch {
-	private String searchName;
-	private long searchID;
-	private double searchCost;
-	private String searchTag;
-	private String searchType;
-	private String searchPartnerName;
-	private String searchPartnerID;
+	private List<Product> products;
+	private Product product;
 	
-	private boolean order;
 	
-	public void setSearchName(String searchName) {
-		this.searchName = searchName;
-	}
-	public void setSearchID(long searchID) {
-		this.searchID = searchID;
-	}
-	public void setSearchCost(double searchCost) {
-		this.searchCost = searchCost;
-	}
-	public void setSearchTag(String searchTag) {
-		this.searchTag = searchTag;
-	}
-	public void setSearchType(String searchType) {
-		this.searchType = searchType;
-	}
-	public void setSearchPartnerName(String searchPartnerName) {
-		this.searchPartnerName = searchPartnerName;
-	}
-	public void setSearchPartnerID(long searchPartnerID) {
-		this.searchPartnerID = searchPartnerID;
-	}	
-	
-	public String getSearchName() {
-		return this.sortName;
-	}
-	public long getSearchID() {
-		return this.sortID;
-	}
-	public double getSearchCost() {
-		return this.sortCost;
-	}
-	public String getSearchTag() {
-		return this.sortTag;
-	}
-	public String getSearchType() {
-		return this.sortType;
-	}
-	public String getSearchPartnerName() {
-		return this.sortPartnerName;
-	}
-	public long getSearchPartnerID() {
-		return this.sortPartnerID;
-	}	
-	
-	public List<Product> productSearchByName(List<Product> products, String searchName, boolean order){
+	public List<Product> productSearchByName(String searchName){
 		List<Product> productNames = new ArrayList<Product>();
-		 
-		for (Product product : products) {
-			if (product.getProductName().equalsIgnoreCase(searchName)) {
-				productNames.add(product);
-			}
-		}
+		//add logic for retrieving products from the database and adding them to the list
+		
 		return productNames;
 	}
-	public Product productSearchByID(List<Product> products, long searchID, boolean order){
-		for (Product product : products) {
-			if (product.getProductID().equalsIgnoreCase(searchID)) {
-				return product;
-			}
-		}
-		return null;
+	public Product productSearchByID(long searchID){
+		//add logic for retrieving product from the database
+				return product;	
 	}
-	public List<Product> productSearchByCost(List<Product> products, double searchCost, boolean order){
+	
+	public List<Product> productSearchByCost(double searchCost){
 		List<Product> productCosts = new ArrayList<Product>();
-		 
-		for (Product product : products) {
-			if (product.getProductCost().equalsIgnoreCase(searchCost)) {
-				productCosts.add(product);
-			}
-		}
+		//add logic for retrieving products from the database and adding them to the list
+		
 		return productCosts;
 	}
-	public List<Product> productSearchByTag(List<Product> products, String searchTag, boolean order){
+	public List<Product> productSearchByTag(String searchTag){
 		List<Product> productTags = new ArrayList<Product>();
-		 
-		for (Product product : products) {
-			if (product.getProductTag().equalsIgnoreCase(searchTag)) {
-				productTags.add(product);
-			}
-		}
+		 //add logic for retrieving products from the database and adding them to the list
 		return productTags;
 	}
 	
-	public List<Product> productSearchByType(List<Product> products, String searchType, boolean order){
+	public List<Product> productSearchByType(String searchType){
 		List<Product> productTypes = new ArrayList<Product>();
-		 
-		for (Product product : products) {
-			if (product.getProductType().equalsIgnoreCase(searchType)) {
-				productTypes.add(product);
-			}
-		}
+		 //add logic for retrieving products from the database and adding them to the list
+		
 		return productTypes;
 	}
-	public List<Product> productSearchByPartnerName(List<Product> products, String searchPartnerName, boolean order){
+	public List<Product> productSearchByPartnerName(String searchPartnerName){
 		List<Product> productPartnerNames = new ArrayList<Product>();
-		 
-		for (Product product : products) {
-			if (product.getProductPartnerName().equalsIgnoreCase(searchPartnerName)) {
-				productPartnerNames.add(product);
-			}
-		}
+		 //add logic for retrieving products from the database and adding them to the list
+		
 		return productPartnerNames;
 	}
-	public List<Product> productSearchByPartnerID(List<Product> products, long searchPartnerID, boolean order){
+	
+	public List<Product> productSearchByPartnerID(long searchPartnerID){
 		List<Product> productPartnerIDs = new ArrayList<Product>();
-		 
-		for (Product product : products) {
-			if (product.getProductPartnerID().equalsIgnoreCase(searchPartnerID)) {
-				productPartnerIDs.add(product);
-			}
-		}
+		 //add logic for retrieving products from the database and adding them to the list
+	
 		return productPartnerIDs;
 	}
 }
