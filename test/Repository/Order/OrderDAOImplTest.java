7b7491ab88721811d3b41f592b07d1f85f98c7f1
Nathnael Alemu <nathnael.alemu@yahoo.com>
2017-10-22 21:22:42 -0500

    added cxf, jackson libraries and created web.xml and other config files

--- test/Repository/Order/OrderDAOImplTest.java
+++ test/Repository/Order/OrderDAOImplTest.java
@@ -1,12 +1,13 @@
 package Repository.Order;
 
 import org.junit.jupiter.api.Test;
+
 import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 
 
 
-@RunWith(MockitoJUnitRunner.class)
+
+
 class OrderDAOImplTest {
 
 
