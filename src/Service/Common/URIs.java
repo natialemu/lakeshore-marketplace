91c12055400975f31a9fa4ca999a0cdb94532445
ljohnson <ljohnson@luc.edu>
2017-12-02 01:09:26 -0600

    URI
    added URIs

--- src/Service/Common/URIs.java
+++ src/Service/Common/URIs.java
@@ -31,9 +31,45 @@ public class URIs {
 	public static final String GET_TRANSACTION = "";
 	public static final String PROVIDE_DELIVERY_STATUS = "";
 	
-	public static final String LOG_IN = "";
-	public static final String REGISTER = "";
+	public static final String ACCOUNT = "http://localhost:8081/account-service/account";
+	public static final String LOG_IN_USERNAME = "http://localhost:8081/account-service/account/login/username";
+	public static final String LOG_IN_EMAIL = "http://localhost:8081/account-service/account/login/email";
+	public static final String USERNAME = "http://localhost:8081/account-service/account/username";
+	public static final String PASSWORD "http://localhost:8081/account-service/account/password";
+	public static final String ACCOUNT ="http://localhost:8081/account-service/account";
 	
+	public static final String BANKNAME ="http://localhost:8081/bank-service/bank/name";
+	public static final String ACCOUNTNUMBER = "http://localhost:8081/bank-service/bank/accountnumber";
+	public static final String ROUTINGNUMBER = "http://localhost:8081/bank-service/bank/routingnumber";
+	public static final String SWIFTCODE = "http://localhost:8081/bank-service/bank/swiftcode;
+	public static final String ACCOUNTTYPE = "http://localhost:8081/bank-service/bank/accounttype";
+	public static final String CARDNUMBER = "http://localhost:8081/bank-service/bank/cardnumber";
+	public static final String CARDEXPIRATION = "http://localhost:8081/bank-service/bank/cardexpiration";
+	public static final String CARDNAME = "http://localhost:8081/bank-service/bank/cardname";
+	public static final String SECURITYCODE = "http://localhost:8081/bank-service/bank/securitycode";
+	public static final String BANKINFO = "http://localhost:8081/bank-service/bank";
+	
+	public static final String EMAIL = "http://localhost:8081/profile-service/profile/email";
+	public static final String FULLNAME = "http://localhost:8081/profile-service/profile/fullname";
+	public static final String BIRTHDATE = "http://localhost:8081/profile-service/profile/birthdate";
+	public static final String LOCATION = "http://localhost:8081/profile-service/profile/phone";
+	public static final String SECURITYQUESTION = "http://localhost:8081/profile-service/profile/securityquestion";
+	public static final String SECURITYANSWER = "http://localhost:8081/profile-service/profile/securityanswer";
+	public static final String PERSONALINFO = "http://localhost:8081/profile-service/profile";
+	public static final String PHONE = "http://localhost:8081/profile-service/profile/phone";
+	
+	public static final String DELIVERYSTATUS = "http://localhost:8081/delivery-service/delivery/"
+	
+	public static final String ORDER = "http://localhost:8081/order-service/order";
+	public static final String ORDERSTATUS = "http://localhost:8081/order-service/orderstatus";
+	public static final String RECENTORDERS = "http://localhost:8081/order-service/recentorders";
+	
+	public static final String PRODUCTBYCOST = "http://localhost:8081/product-service/filter/products/productbycost";
+	public static final String PRODUCTBYRATING = "http://localhost:8081/product-service/filter/products/productbyrating";
+	public static final String PRODUCTBYDATE = "http://localhost:8081/product-service/filter/products/productbydate";
+	public static final String PRODUCTBYSELLER = "http://localhost:8081/product-service/filter/products/productbyseller";
+	
+	public static final String PRODUCTS = "http://localhost:8081/product-service/products";
+	
+	}	
 
-
-}
